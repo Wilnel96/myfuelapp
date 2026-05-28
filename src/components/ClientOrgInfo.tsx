9bec964a3ffd5faa6045078abb2520900576baf0
Wilnel96 <96678708+Wilnel96@users.noreply.github.com>
2026-05-29 00:04:15 +0200

    Updated ClientOrgInfo.tsx

--- src/components/ClientOrgInfo.tsx
+++ src/components/ClientOrgInfo.tsx
@@ -311,16 +311,18 @@ export default function ClientOrgInfo({ onNavigate, clientSelfMode = false }: Cl
         </div>
       )}
 
-      <div className="relative">
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-        <input
-          type="text"
-          placeholder="Search organizations..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-      </div>
+      {!clientSelfMode && (
+        <div className="relative">
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+          <input
+            type="text"
+            placeholder="Search organizations..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          />
+        </div>
+      )}
 
       <div className="space-y-2">
         {filteredOrganizations.map((org) => {
